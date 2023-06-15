15bed7ea5c834e71f5258083a68172fe2ac99523
Mihai Neagoe <63103949+Gundalf21@users.noreply.github.com>
2023-06-15 14:49:52 +0200

    Add files via upload

---
+++ ProiectJoc2D/src/com/company/AssetSetter.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import entity.NPC_Dryad;
+
+public class AssetSetter {
+    GamePanel gp;
+
+    public AssetSetter(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void setNPC() {
+        gp.npc[0] = new NPC_Dryad(gp);
+        gp.npc[0].worldX = gp.tileSize*21;
+        gp.npc[0].worldY = gp.tileSize*21;
+    }
+}
---
+++ ProiectJoc2D/src/entity/Entity.java
@@ -0,0 +1,47 @@
+package entity;
+
+import com.company.GamePanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Entity {
+
+    GamePanel gp;
+    public int worldX, worldY;
+    public int speed;
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
+
+    public Entity(GamePanel gp){
+        this.gp = gp;
+    }
+
+    public void draw(Graphics2D g2, GamePanel gp){
+        BufferedImage image = null;
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+           worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+           worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+           worldY - gp.tileSize < gp.player.worldY + gp.player.screenY){
+
+            switch (direction) {
+                case "down":
+                    if (spriteNum == 1) {
+                        image = up1;
+                    }
+                    if (spriteNum == 2) {
+                        image = up2;
+                    }
+                    break;
+            }
+            g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+        }
+    }
+}
